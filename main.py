8016573de976048193a76b0c3ac62a1d66caf5d6
Erick Urias <erickuriasb@gmail.com>
2022-06-14 12:11:28 -0600

    Read csv file and do the report

---
+++ main.py
@@ -0,0 +1,48 @@
+import pandas as pd
+
+from fastapi import FastAPI, status, File, UploadFile
+from pydantic import EmailStr
+
+app = FastAPI()
+
+
+def process_transactions(df):
+    months = {
+        "1": "January", "2": "Febrary", "3": "March", "4": "April", "5": "May",
+        "6": "June", "7": "July", "8": "August", "9": "September", "10": "October",
+        "11": "November", "12": "December"
+    }
+    report = {
+        "Total_Balance": df['Transaction'].sum()
+    }
+    Debit_Amount = 0
+    Credit_Amount = 0
+    Count_Debit_Transactions = 0
+    Count_Credit_Transactions = 0
+    transactions_by_month = {}
+    for row in range(len(df)):
+        transaction = df.loc[row].to_dict()
+        if transaction["Transaction"] < 0:
+            Debit_Amount += transaction["Transaction"]
+            Count_Debit_Transactions += 1
+        else:
+            Credit_Amount += transaction["Transaction"]
+            Count_Credit_Transactions += 1
+        if months[transaction["Date"][0]] in transactions_by_month:
+            transactions_by_month[months[transaction["Date"][0]]] += 1
+        else:
+            transactions_by_month[months[transaction["Date"][0]]] = 1
+
+    report["Average_Debit_Amount"] = Debit_Amount / Count_Debit_Transactions
+    report["Average_Credit_Amount"] = Credit_Amount / Count_Credit_Transactions
+    #report["Transactions_By_Month"] = [{key: value} for key,value in transactions_by_month.items()]
+    report["Transactions_By_Month"] = transactions_by_month
+
+    return report
+
+
+@app.post('/file_processing/')
+async def read_csv_file(email: EmailStr, file: UploadFile):
+    df = pd.read_csv(file.file)
+    print(email)
+    return process_transactions(df)
